f0c09f4aa0442e6138213f5c30b3675174aaf314
Jacob Gronlund <jacobgronlund@Jacobs-MacBook-Pro-3.local>
2024-11-25 17:56:49 -0800

    adding more to image_encoder/runner and created recipe_encoder

--- models/image_encoder.py
+++ models/image_encoder.py
@@ -8,21 +8,22 @@ import torch.functional as F
 # processor = CLIPProcessor.from_pretrained(model_name)
 
 
-
-class ImageEncoder(nn.Module):
+class Image2Recipe(nn.Module):
     def __init__(self, clip_model, num_classes):
-        super(ImageEncoder, self).__init__()
+        super(Image2Recipe, self).__init__()
+
         self.parent_model = clip_model
         self.image_embedding_dim = clip_model.config.hidden_size
         self.fc1 = nn.Linear(self.image_embedding_dim, num_classes)
 
-    def forward(self, images, image_titles):
+    def forward(self, images, image_labels):
         image_encodings = self.parent_model.get_image_features(pixel_values=images)
         # image_encodings = F.normalize(dim=-1, keepdim=True)
         image_encodings = F.normalize(image_encodings, p=2, dim=-1)
-        text_features = self.clip_model.get_text_features(input_ids=image_titles['input_ids'],
-                                                          attention_mask=image_titles['attention_mask'])
+        text_features = self.clip_model.get_text_features(input_ids=image_labels['input_ids'],
+                                                          attention_mask=image_labels['attention_mask'])
         text_features = F.normalize(text_features, p=2, dim=-1)
+        
         image_logits = self.fc1(image_encodings)
 
 
---
+++ models/recipe_encoder.py
@@ -0,0 +1,11 @@
+import torch.nn as nn
+
+
+class Recipe_Encoder(nn.Module):
+    def __init__(self):
+        super(Recipe_Encoder, self).__init__()
+
+    def forward(self, token_inputs):
+        outputs = None
+
+        return outputs
